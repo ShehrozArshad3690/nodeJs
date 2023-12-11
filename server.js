3355a4463acddd4ee651ddc5e8f556dbd536be4d
Shehroz Rajpoot <Shehrozarshad233@gmail.com>
2023-12-11 14:45:29 +0500

    added some comments

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 let http =require('http');
 
-
+// created a server
 let server=http.createServer((req,res)=>{
     console.log("Request has been made");
 })
 
+// listens the server on specific port
 server.listen(443,'localhost',()=>{
     console.log("Server is listening for requests on port 443");
 })
