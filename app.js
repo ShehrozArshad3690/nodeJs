d4fbc65984a54238b6eb904a6139082a4384c21d
Shehroz Rajpoot <Shehrozarshad233@gmail.com>
2023-12-12 11:12:21 +0500

    install ejs & set view engine as ejs in app.js created some ejs files in views and partials

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 //express app
 const app =express();
 
+// register view engine
+app.set('view engine','ejs');
+
 //listen for request
 app.listen(8080);
 
@@ -10,18 +13,31 @@ app.listen(8080);
 app.get('/',(req,res)=>{
     // res.send('Hello World!');                                    //getting string response
     // res.send(`<p>This is my first express app</p>`);             //getting string response
-    res.sendFile('./views/index.html',{root:__dirname});
+    // res.sendFile('./views/index.html',{root:__dirname});
+    const Blogs=[
+        {title:'Earth',snippet:'lorem ipsumkj  sdkf jsdlf jsldf '},
+        {title:'Mars',snippet:'lorem ipsumkj  sdkf jsdlf jsldf '},
+        {title:'Sun',snippet:'lorem ipsumkj  sdkf jsdlf jsldf '}
+    ];
+    res.render('index' , {title:'Home',Blogs});
 })
 
 app.get('/about',(req,res)=>{
-    res.sendFile('./views/about.html',{root:__dirname});
+    // res.sendFile('./views/about.html',{root:__dirname});
+    res.render('about' , {title: 'About'});
 })
+
+app.get('/blogs/create',(req,res)=>{
+    res.render('create' , {title: 'Create a new blog'});
+})
+
 // route for redirect
 app.get('/aboutus',(req,res)=>{
-    res.redirect('/about');
+    res.redirect('/about' , {title: 'About'});
 })
 
 // route for 404 page
 app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html',{root:__dirname});              // here we manually tell function status code of 404 because it doesn't detect it an error
+    // res.status(404).sendFile('./views/404.html',{root:__dirname});              // here we manually tell function status code of 404 because it doesn't detect it an error
+    res.status(404).render('404' , {title: '404'});
 })
