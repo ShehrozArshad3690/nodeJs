af6d4629b44131a5b14e36e158bcd847392607d3
Shehroz Rajpoot <Shehrozarshad233@gmail.com>
2023-12-13 14:35:58 +0500

    add controller of mvc

---
+++ controllers/blogControllers.js
@@ -0,0 +1,50 @@
+const Blog = require("../models/blog");
+
+// blog_index, blog_details, blog_create_get, blog_create_post, blog_delete
+
+const blog_index = (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", Blogs: result });
+    })
+    .catch((error) => console.log(error));
+};
+
+const blog_details = (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: "Blog Details" });
+    })
+    .catch((error) => console.log(error));
+};
+
+const blog_create_get = (req, res) => {
+  res.render("create", { title: "Create a new blog" });
+};
+
+const blog_create_post = (req, res) => {
+  // console.log(req.body);
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then((result) => res.redirect("/blogs"))
+    .catch((error) => console.log(error));
+};
+
+const blog_delete = (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/blogs" });
+    })
+    .catch((error) => console.log(error));
+};
+module.exports = {
+  blog_index,
+  blog_details,
+  blog_create_get,
+  blog_create_post,
+  blog_delete
+};
